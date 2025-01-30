9da78dd56c7c6b6f1a10717d7fb8828d4aa6b87b
Josep Ferrer <josepferrer@macbookpro.lan>
2025-01-30 09:38:06 +0100

    removed print

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 def pytest_configure(config):
     """Load environment variables at the start of tests"""
     load_dotenv()
-    print("pytest_configure")
-    print(os.getenv('GITHUB_TOKEN'))
+
     # Verify GitHub token exists
     if not os.getenv('GITHUB_TOKEN'):
         raise ValueError(

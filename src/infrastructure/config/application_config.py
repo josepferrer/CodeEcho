cdc5a91276d5fa6cfe66a9e5beec6426f56827e0
Josep Ferrer Boix <josep.ferrer.boix@gmail.com>
2025-02-08 18:47:09 +0100

    Configured logging

--- src/infrastructure/config/application_config.py
+++ src/infrastructure/config/application_config.py
@@ -1,9 +1,18 @@
 from pathlib import Path
+import logging
 
 class ApplicationConfig:
     def __init__(self, github_token: str = None, base_data: str = "data"):
         self.base_data = base_data or "data"
         self.github_token = github_token
+        logging.basicConfig(
+            level=logging.INFO,  # Cambia a DEBUG si necesitas más detalle
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            handlers=[
+                         # logging.StreamHandler(sys.stdout),
+                         logging.FileHandler(base_data + "/app.log"),
+                     ]
+        )
 
     @property
     def pull_request_db_path(self) -> Path:
